9a6d50978d0e91fe6e55e1ec26178aae04cb00d5
Jackson <jackson.roque98@icloud.com>
2023-10-17 20:06:51 -0300

    final project

---
+++ src/main/java/estacionamento/domain/model/NumberAcess.java
@@ -0,0 +1,19 @@
+package estacionamento.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_numberAcess")
+public class NumberAcess {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long numberAcess;
+    public Long getNumberAcess() {
+        return numberAcess;
+    }
+    public void setNumberAcess(Long numberAcess) {
+        this.numberAcess = numberAcess;
+    }
+}
---
+++ src/main/java/estacionamento/domain/model/User.java
@@ -0,0 +1,47 @@
+package estacionamento.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String cpf;
+    @OneToOne(cascade = CascadeType.ALL)
+    private NumberAcess numberAcess;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Car car;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public NumberAcess getNumberAcess() {
+        return numberAcess;
+    }
+    public void setNumberAcess(NumberAcess numberAcess) {
+        this.numberAcess = numberAcess;
+    }
+    public Car getCar() {
+        return car;
+    }
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
